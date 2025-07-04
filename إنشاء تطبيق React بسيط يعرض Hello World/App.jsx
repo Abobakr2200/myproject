b81e044c7bf24f6233d08441a7b87eac80c487bc
abobakrgamal <gaabobakr1@gmail.com>
2025-07-04 16:29:11 +0300

    إنشاء تطبيق React بسيط يعرض Hello World

---
+++ إنشاء تطبيق React بسيط يعرض Hello World/App.jsx
@@ -0,0 +1,13 @@
+import HelloWorld from './HelloWorld.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App
+
---
+++ إنشاء تطبيق React بسيط يعرض Hello World/HelloWorld.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * مكون HelloWorld يعرض رسالة "Hello World"
+ * @returns {JSX.Element} عنصر JSX يحتوي على النص
+ */
+function HelloWorld() {
+  return (
+    <h1 className="hello-world-title">
+      Hello World
+    </h1>
+  );
+}
+
+export default HelloWorld;
+
